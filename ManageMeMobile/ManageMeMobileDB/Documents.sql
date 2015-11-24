23396b0a4814132eb3158b05b66ef90961e70ed0
Feng Pan <wadepan@hotmail.com>
2015-11-23 19:42:02 -0500

    add database project

---
+++ ManageMeMobile/ManageMeMobileDB/Documents.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Documents]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(250) NULL, 
+    [Path] NVARCHAR(250) NULL, 
+    [Date] DATE NULL, 
+    [Notes] NVARCHAR(500) NULL
+)
